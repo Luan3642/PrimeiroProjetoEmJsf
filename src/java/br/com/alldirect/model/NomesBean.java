f64f41dff20e5007b6f29699dbffa5f8c731f0b2
Aluno <Aluno@192.68.0.57>
2016-12-05 16:57:15 -0200

    CampoNomesSobrenomes

---
+++ src/java/br/com/alldirect/model/NomesBean.java
@@ -0,0 +1,13 @@
+package br.com.alldirect.model;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean
+
+public class NomesBean {  
+    private String vadia;
+    private String sobrenomeDaVadia;
+}
